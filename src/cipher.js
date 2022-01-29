6e6a1ec85aad499dae3ebc4ee08b5a770b1de92e
Amanda Gusmão <engamandagusmao@gmail.com>
2022-01-28 23:01:12 -0300

    funcionalidade da cifra

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
+const alfabeto = [
+  'A', 'B', 'C', 'D', 'E', 'F',
+  'G', 'H', 'I', 'J', 'K', 'L',
+  'M', 'N', 'O', 'P', 'Q', 'R',
+  'S', 'T', 'U', 'V', 'W', 'X',
+  'Y', 'Z'
+];
+
 const cipher = {
-  // ...
+  encode: function(offset, text) {
+    return [...text].map(char => cipher.encodeChar(offset, char)).join('')
+  },
+  decode: function(offset, text) {
+    return [...text].map(char => cipher.decodeChar(offset, char)).join('')
+  },
+  encodeChar: function(inputOffset, char) {
+    const offset = Number (inputOffset) % alfabeto.length;
+      if (alfabeto.includes(char.toUpperCase())) {
+        const position = alfabeto.indexOf(char.toUpperCase());
+        const newPosition = (offset + position) % alfabeto.length;
+        return alfabeto[newPosition];
+      }else return char;
+  },
+  decodeChar: function(inputOffset, char) {
+    if (alfabeto.includes(char.toUpperCase())) {
+      const position = alfabeto.indexOf(char.toUpperCase());
+      const offset = (Number (inputOffset) - position) % alfabeto.length;
+      const newPosition = ((offset - alfabeto.length) * -1) % alfabeto.length;
+      return alfabeto[newPosition];
+    }else return char;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const form = document.forms[0];
+
+const resultadoTexto = document.getElementById('resultadoTexto');
+
+document.querySelector('#clickMe').addEventListener('click', function() {
+  event.preventDefault();
+
+  const select = document.querySelector('#escolha').value;
+
+  const desloc = document.querySelector('#offset').value;
+
+  const text = document.querySelector('#meuTexto').value;
+
+  console.log(select, desloc, text);
+
+  resultadoTexto.innerHTML = cipher.encode(form.offset.value, form.meuTexto.value);
+
+}, false);
+
